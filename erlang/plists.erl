222539cb1c3be5397e23491cb51a2eeaf920c629
Erik Søe Sørensen <ess@trifork.com>
2012-02-08 14:30:24 +0100

    Erlang: optimize plist: (1) start workers as soon as possible, (2) don't count list length more than once. (Looks like a ~10% improvement.)

--- erlang/plists.erl
+++ erlang/plists.erl
@@ -5,21 +5,22 @@
 
 % Algorithm - Split into 4 lists, have each list and the fun run in a separate spawned process
 foreach(Fun,List) -> 
-	Splits = split(8,List),
-	Pids = execute(Splits,Fun,self()),
+        Pids = split_and_execute(8, List, Fun, self()),
 	waitCompletion(Pids).
 
-split(1,List) -> [List];
-split(Pieces,List) -> 
-	Length = length(List),
-	N = round(Length/Pieces),
-	{List1,Remainder} = lists:split(N,List),
-	[List1|split(Pieces-1,Remainder)].
+split_and_execute(Pieces,List,Fun,EndPid) ->
+    PieceSize = round(length(List) / Pieces),
+    split_and_execute(Pieces, PieceSize, List, Fun, EndPid).
+    
+split_and_execute(1,_PieceSize,List,Fun,EndPid) ->
+    [execute(List, Fun, EndPid)];
+split_and_execute(Pieces,PieceSize,List,Fun,EndPid) -> 
+	{List1,Remainder} = lists:split(PieceSize,List),
+	[execute(List1, Fun, EndPid)
+	 | split_and_execute(Pieces-1,PieceSize,Remainder,Fun,EndPid)].
 
-execute([],_Fun,_EndPid) -> [];
-execute([L|Lists],Fun,EndPid) -> 
-	Pid = spawn( fun()->runFun(Fun,L,EndPid) end ),
-	[Pid|execute(Lists,Fun,EndPid)].
+execute(List,Fun,EndPid) -> 
+    spawn( fun()->runFun(Fun,List,EndPid) end ).
 
 runFun(Fun,List,EndPid) -> lists:foreach(Fun,List),
 	EndPid ! finished.
